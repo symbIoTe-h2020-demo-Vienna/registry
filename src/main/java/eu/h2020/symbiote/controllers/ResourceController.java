21154e950926041e41b5697d14e545f8fcabf904
Mateusz Łukaszenko <mateusz.lukaszenko@man.poznan.pl>
2016-09-26 14:47:41 +0200

    HATEOAS removed for now. Endpoints updated to concept

--- src/main/java/eu/h2020/symbiote/controllers/ResourceController.java
+++ src/main/java/eu/h2020/symbiote/controllers/ResourceController.java
@@ -6,7 +6,6 @@ import eu.h2020.symbiote.model.Sensor;
 import eu.h2020.symbiote.repository.PlatformRepository;
 import eu.h2020.symbiote.repository.SensorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.rest.webmvc.RepositoryRestController;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,10 +14,9 @@ import org.springframework.web.bind.annotation.*;
 /**
  * Created by mateuszl on 22.09.2016.
  */
-
-@CrossOrigin(methods = RequestMethod.POST)
-@RepositoryRestController
-public class SensorController {
+@CrossOrigin
+@RestController
+public class ResourceController {
 
     @Autowired
     private SensorRepository sensorRepo;
@@ -26,7 +24,22 @@ public class SensorController {
     @Autowired
     private PlatformRepository platformRepo;
 
-    @RequestMapping(value="/platform/{id}/sensors", method= RequestMethod.POST)
+    @RequestMapping(value="/register/platform", method= RequestMethod.POST)
+    public @ResponseBody
+    HttpEntity<String> addPlatform(@RequestBody Platform platform) {
+        System.out.println( "Adding Platform");
+        Platform savedPlatform = platformRepo.save(platform);
+
+        System.out.println( "Platform added! : " + savedPlatform + ". Sending message...");
+        //Sending message
+        RegistrationPublisher.getInstance().sendPlatformCreatedMessage( savedPlatform );
+
+//        return new ResponseEntity<Platform>( savedPlatform, HttpStatus.OK);
+        System.out.println("Response send with id: " + savedPlatform.getId());
+        return new ResponseEntity<String>( savedPlatform.getId(), HttpStatus.OK);
+    }
+
+    @RequestMapping(value="/register/platform/{id}/resources", method= RequestMethod.POST)
     public @ResponseBody
     HttpEntity<String> addSensor(@PathVariable(value="id") String platformId, @RequestBody Sensor sensor) {
         System.out.println( "Adding Sensor");
@@ -44,4 +57,12 @@ public class SensorController {
         System.out.println("Response send with id: " + savedSensor.getId());
         return new ResponseEntity<String>( savedSensor.getId(), HttpStatus.OK);
     }
+
+    @RequestMapping(value={"/sensor","/platform"})
+    public @ResponseBody
+    HttpEntity<String> error() {
+        String message = "Error. Method not allowed";
+        return new ResponseEntity<String>( message, HttpStatus.METHOD_NOT_ALLOWED);
+    }
+
 }
