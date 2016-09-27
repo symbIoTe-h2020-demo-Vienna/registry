8ce72841a860c511993c3da27bd92f66643ae6e6
Mateusz Łukaszenko <mateusz.lukaszenko@man.poznan.pl>
2016-09-27 12:39:29 +0200

    minor changes in endpoints

--- src/main/java/eu/h2020/symbiote/controllers/ResourceController.java
+++ src/main/java/eu/h2020/symbiote/controllers/ResourceController.java
@@ -5,6 +5,8 @@ import eu.h2020.symbiote.model.Platform;
 import eu.h2020.symbiote.model.Sensor;
 import eu.h2020.symbiote.repository.PlatformRepository;
 import eu.h2020.symbiote.repository.SensorRepository;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
@@ -18,51 +20,59 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class ResourceController {
 
+    public static Log log = LogFactory.getLog(ResourceController.class);
+
     @Autowired
     private SensorRepository sensorRepo;
 
     @Autowired
     private PlatformRepository platformRepo;
 
-    @RequestMapping(value="/cloud_api/platforms", method= RequestMethod.POST)
-    public @ResponseBody
+    @RequestMapping(value = "/cloud_api/platforms", method = RequestMethod.POST)
+    public
+    @ResponseBody
     HttpEntity<String> addPlatform(@RequestBody Platform platform) {
-        System.out.println( "Adding Platform");
+        log.debug("Adding Platform");
+
+        //TODO check if provided platform already exists
+
         Platform savedPlatform = platformRepo.save(platform);
-
-        System.out.println( "Platform added! : " + savedPlatform + ". Sending message...");
+        log.debug("Platform added! : " + savedPlatform + ". Sending message...");
         //Sending message
-        RegistrationPublisher.getInstance().sendPlatformCreatedMessage( savedPlatform );
+        RegistrationPublisher.getInstance().sendPlatformCreatedMessage(savedPlatform);
 
-//        return new ResponseEntity<Platform>( savedPlatform, HttpStatus.OK);
-        System.out.println("Response send with id: " + savedPlatform.getId());
-        return new ResponseEntity<String>( savedPlatform.getId(), HttpStatus.OK);
+        log.debug("Response send with id: " + savedPlatform.getId());
+        return new ResponseEntity<String>(savedPlatform.getId(), HttpStatus.OK);
     }
 
-    @RequestMapping(value="/cloud_api/platforms/{platform_id}/resources", method= RequestMethod.POST)
-    public @ResponseBody
-    HttpEntity<String> addSensor(@PathVariable(value="platform_id") String platformId, @RequestBody Sensor sensor) {
-        System.out.println( "Adding Sensor");
+    @RequestMapping(value = "/cloud_api/platforms/{platform_id}/resources", method = RequestMethod.POST)
+    public
+    @ResponseBody
+    HttpEntity<String> addSensor(@PathVariable(value = "platform_id") String platformId, @RequestBody Sensor sensor) {
+        log.debug("Adding Sensor...");
+        Platform foundPlatform = platformRepo.findOne(platformId);
 
-        Platform foundPlatform = platformRepo.findOne(platformId.toString());
+        if (foundPlatform != null) {
+            sensor.setPlatform(foundPlatform);
+            Sensor savedSensor = sensorRepo.save(sensor);
+            log.debug("Sensor added! : " + savedSensor + ". Sending message...");
+            //Sending message
+            RegistrationPublisher.getInstance().sendSensorCreatedMessage(savedSensor);
 
-        sensor.setPlatform(foundPlatform);
-
-        Sensor savedSensor = sensorRepo.save(sensor);
-        System.out.println( "Sensor added! : " + savedSensor + ". Sending message...");
-        //Sending message
-        RegistrationPublisher.getInstance().sendSensorCreatedMessage( savedSensor );
-
-//        return new ResponseEntity<Sensor>( savedSensor, HttpStatus.OK);
-        System.out.println("Response send with id: " + savedSensor.getId());
-        return new ResponseEntity<String>( savedSensor.getId(), HttpStatus.OK);
+            log.debug("Response send with id: " + savedSensor.getId());
+            return new ResponseEntity<String>(savedSensor.getId(), HttpStatus.OK);
+        } else {
+            log.debug("Platform with provided ID not found!");
+            return new ResponseEntity<String>("Platform with provided ID not found!", HttpStatus.NOT_FOUND);
+        }
     }
 
-    @RequestMapping(value={"/sensor","/platform"})
-    public @ResponseBody
+    @RequestMapping(value = {"/sensor", "/sensors", "/platform", "/platforms", "/cloud_api", "/cloud_api/sensors"})
+    public
+    @ResponseBody
     HttpEntity<String> error() {
         String message = "Error. Method not allowed";
-        return new ResponseEntity<String>( message, HttpStatus.METHOD_NOT_ALLOWED);
+        return new ResponseEntity<String>(message, HttpStatus.METHOD_NOT_ALLOWED);
     }
 
 }
